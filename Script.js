5cd30fab5900a0fe1a35fbbc9d8c3e39db8e5d86
Luiz <luiz.schastai@escola.pr.gov.br>
2026-07-09 10:36:10 -0300

    teste

--- Script.js
+++ Script.js
@@ -1,23 +1,19 @@
-const botoes = document.querySelectorAll("article button");
+const botoes = document.querySelectorAll("button");
 
 botoes.forEach(function (botao) {
     let curtiu = false;
 
-    botao.addEventListener("click", function () {
-        const contador = botao.querySelector("span");
+    botao.addEventListener("click", botaoClicado);
 
-        if (curtiu) {
-            contador.textContent--;
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
         } else {
-            contador.textContent++;
+            texto.textContent--;
+            curtiu = false;
         }
-
-        curtiu = !curtiu;
-    });
-});
-
-const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
-
-btnTemaEscuro.addEventListener("click", function () {
-    document.body.classList.toggle("tema-escuro");
+    }
 });
